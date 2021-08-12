f794e9b44512fffb996111232d7b6300a7d451fc
Viviana Bermúdez <vivi218@hotmail.com>
2021-08-12 12:57:16 -0300

    probando cambios
    cambios para traer json productos

--- js/products.js
+++ js/products.js
@@ -1,3 +1,28 @@
+function showProductsList(){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let category = currentCProductsArray[i];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
