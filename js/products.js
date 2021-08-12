9a26346339103b53bb054f43543efa32ee5892ed
Viviana Bermúdez <vivi218@hotmail.com>
2021-08-12 08:38:48 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,5 +2,12 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showCategoriesList(categoriesArray);
+        }
+    });
 });
