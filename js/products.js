a6c0b3d769acd4470b317dfa8d71bab96f0e958b
Viviana Bermúdez <vivi218@hotmail.com>
2021-08-13 10:43:03 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,39 +1,31 @@
-var categoriesArray = [];
-
-function showProductsList(){
+function showProductList(){
 
     let htmlContentToAppend = "";
-    for(let i = 0; i < currentProductsArray.length; i++){
-        let products = currentProductsArray[i];
-htmlContentToAppend += `
-<tr>
-                <td>` + currentProductsArray[i].name + `</td>
-                <td>`+ currentProductsArray[i].cost + `</td>
-                <td>`+ currentProductsArray[i].description + `</td>
+    for(let i = 0; i < currentProductArray.length; i++){
+        let product = currentProductArray[i];
+        htmlContentToAppend += `
+        <a href="category-info.html" class="list-group-item list-group-item-action">
+        <div class="row">
+            <div class="col-3">
+                <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+            </div>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1">`+product.name +`</h4>
+                    <small class="text-muted">` + product.productCount + ` artículos</small>
+                </div>
+                <p class="mb-1">` + product.description + `</p>
+            </div>
+        </div>
+    </a>
+    `
+ }
+
+ document.getElementById("results").innerHTML = htmlContentToAppend;
+}
+
             
-                </tr> 
-
-            `;
-        }
-
-        document.getElementById("results").innerHTML = htmlContentToAppend;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -42,9 +34,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function(resultObj){
         if (resultObj.status === "ok")
         {
-            categoriesArray = resultObj.data;
+           productArray = resultObj.data;
             //Muestro las categorías ordenadas
-            showCategoriesList(categoriesArray);
+            showProductList(productArray);
         }
     });
 });
