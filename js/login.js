8af86a62d9dace8e8ecb3aef5d82cf2b606667a3
Viviana Bermúdez <vivi218@hotmail.com>
2021-08-13 14:52:24 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,3 +1,18 @@
+function valida(){
+    var usuario = document.getElementById('usuario');
+    var contra = document.getElementById("contraseña");
+    
+    if((usuario.value == "")||(contra.value == "")){
+    window.alert("Los campos usuario y contraseña no pueden estar vacios");
+    }else{
+    envia('login');
+    }
+    }
+
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
