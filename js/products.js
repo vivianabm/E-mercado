4498a2f9d8b43ff686f64854045d645d6f7ce99e
Viviana Bermúdez <vivi218@hotmail.com>
2021-08-13 11:30:07 -0300

    js productos actualizado
    se realizan modificaciones finales a js

--- js/products.js
+++ js/products.js
@@ -1,23 +1,28 @@
-function showProductList(){
+var productsArray = [];
+function showProductsList(array){
 
     let htmlContentToAppend = "";
-    for(let i = 0; i < currentProductArray.length; i++){
-        let product = currentProductArray[i];
-        htmlContentToAppend += `
-        <a href="category-info.html" class="list-group-item list-group-item-action">
-        <div class="row">
-            <div class="col-3">
-                <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-            </div>
-            <div class="col">
-                <div class="d-flex w-100 justify-content-between">
-                    <h4 class="mb-1">`+product.name +`</h4>
-                    <small class="text-muted">` + product.productCount + ` artículos</small>
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
+    htmlContentToAppend += `
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
                 </div>
-                <p class="mb-1">` + product.description + `</p>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                    <div class="mb-1">
+                        <h4 class="mb-1">`+ product.name +`</h4>
+                        <p>` + product.description +`<p>
+                        <p>` + product.cost + product.currency +`<p>
+                        </div>
+                        <small class="text-muted">` + product.soldCount + ` artículos</small>
+                    </div>
+ 
+                </div>
             </div>
         </div>
-    </a>
+      
     `
  }
 
@@ -34,9 +39,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function(resultObj){
         if (resultObj.status === "ok")
         {
-           productArray = resultObj.data;
+           productsArray = resultObj.data;
             //Muestro las categorías ordenadas
-            showProductList(productArray);
+            showProductsList(productsArray);
         }
     });
 });
