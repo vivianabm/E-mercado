33e841d30399d06db22f7ae0c96af43530ec1529
Viviana Bermúdez <vivi218@hotmail.com>
2021-08-19 11:33:19 -0300

    css y js
    se agrega estilo invaid en css y se cambia js login

--- js/login.js
+++ js/login.js
@@ -1,58 +1,33 @@
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function() {
-document.getElementById("submitbotton").addEventListener("clicK", function () {
- let  inputUsuario = document.getElementById ("inputUsuario");
- let  inputClave = document.getElementById ("inputClave");
- let todoCompleto = true ;
-  
- if  ( inputUsuario.value  ===  "" ) {
-  inputUsuario.classList.add("invalid");
-    todoCompleto = false;
-}else{
-  inputUsuario.classList.remove("invalid");
 
-if ( inputClave.value  ===  "" ) {
-  inputClave.classList.add("invalid");
-    todoCompleto = false;
-}else{
-  inputClave.classList.remove("invalid");
-}
-if (todoCompleto){
+  document.getElementById("submitbutton").addEventListener("click", function(e){
+    let inputUsuario = document.getElementById("inputUsuario");
+    let inputClave = document.getElementById("inputClave");
+    let camposCompletos = true;
 
-  if (inputUsuario.value,inputClave.value){
-    window.location= 'inicio.html';
-  }
-  else {
-      alert("Usuario o contraseña incorrectas!")
-  }
-}
-else {
-  alert("Debes ingresar los datos!")
-}
-}
+    if (inputUsuario.value === ''){
+        inputUsuario.classList.add("invalid");
+        camposCompletos = false;
+      }else{
+        inputUsuario.classList.remove("invalid");
+    }
+
+    if (inputClave.value === ''){
+        inputClave.classList.add("invalid");
+        camposCompletos = false;
+      }else{
+        inputClave.classList.remove("invalid");
+    }
+
+    if (camposCompletos){
+        window.location = 'inicio.html';
+    }else{
+        alert("Debes ingresar usuario y contraseña");
+    }
 });
 });
-
-  
-
-   
