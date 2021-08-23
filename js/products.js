412e97a923da477a57fc84d2eec420320605cdc0
Viviana Bermúdez <vivi218@hotmail.com>
2021-08-23 10:07:06 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,39 +1,3 @@
-const ORDER_ASC_BY_NAME = "AZ";
-const ORDER_DESC_BY_NAME = "ZA";
-const ORDER_BY_PROD_COUNT = "Cant.";
-var currentCategoriesArray = [];
-var currentSortCriteria = undefined;
-var minCount = undefined;
-var maxCount = undefined;
-
-function sortCategories(criteria, array){
-    let result = [];
-    if (criteria === ORDER_ASC_BY_NAME)
-    {
-        result = array.sort(function(a, b) {
-            if ( a.name < b.name ){ return -1; }
-            if ( a.name > b.name ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_DESC_BY_NAME){
-        result = array.sort(function(a, b) {
-            if ( a.name > b.name ){ return -1; }
-            if ( a.name < b.name ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_BY_PROD_COUNT){
-        result = array.sort(function(a, b) {
-            let aCount = parseInt(a.productCount);
-            let bCount = parseInt(b.productCount);
-
-            if ( aCount > bCount ){ return -1; }
-            if ( aCount < bCount ){ return 1; }
-            return 0;
-        });
-    }
-
-    return result;
-}
 
 var productsArray = []; // declaro la variable productsArray como un array vacio donde guardo lo que encuentre en el JSON
 function showProductsList(array){ //declaro función
@@ -65,16 +29,15 @@ htmlContentToAppend += `
 
  document.getElementById("results").innerHTML = htmlContentToAppend; //agrego el contenido a "results"
 }
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function(resultObj){ // le pasamos por parámetro la URL
         if (resultObj.status === "ok") // si está todo ok ejecuto el código
         {
-           productsArray = resultObj.data;
-            //Muestro las categorías ordenadas
-            showProductsList(productsArray);
-        }
-    });
+            productsArray = resultObj.data;
+             //Muestro las categorías ordenadas
+             showProductsList(productsArray);
+         }
 });
